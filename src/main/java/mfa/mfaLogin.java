8f4e4d3a066ad029f542f76b4dcc64c57a3235c4
AdityaRawatpr <rawataditya231@gmail.com>
2024-07-18 15:12:13 +0530

    Update mfaLogin.java

--- src/main/java/mfa/mfaLogin.java
+++ src/main/java/mfa/mfaLogin.java
@@ -10,39 +10,8 @@ public class mfaLogin {
     private static final String USER_SECRET_KEY = "***";
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-
-        try {
-            // Open URL
-            String url = "https://github.com/login";  // Replace with the actual URL
-            driver.manage().window().maximize();
-            driver.get(url);
-
-            // Find username and password fields and fill them in
-            String username = "***@nagarro.com";  // Replace with your username
-            String password = "***";  // Replace with your password
-
-            WebElement usernameField = driver.findElement(By.id("login_field"));  // Replace with the actual ID of the username field
-            WebElement passwordField = driver.findElement(By.id("password"));  // Replace with the actual ID of the password field
-
-            usernameField.sendKeys(username);
-            passwordField.sendKeys(password);
-
-            WebElement signinButton = driver.findElement(By.xpath("//input[@type='submit']"));
-            signinButton.click();
-
-            WebElement toptField = driver.findElement(By.id("app_totp"));
-            toptField.sendKeys(readOtp());
-
-
-            // Add a delay to observe the result, if necessary
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            // Close the browser
-            driver.quit();
-        }
+        readOtp()
+        
     }
     static String readOtp(){
         Totp totp = new Totp(USER_SECRET_KEY); // 2FA secret key
